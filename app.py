fb45c5605ffed4999e6b4124159f5a79bb8b6e6d
saifali-007 <itssaifali007@gmail.com>
2025-09-15 13:37:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,23 +8,24 @@ model = pickle.load(open("delhi_price_model.pkl", "rb"))
 st.title("🏠 Delhi House Price Prediction")
 
 # Input fields
-area = st.number_input("Enter Area (sq ft)", min_value=100, max_value=10000, step=50)
-bhk = st.number_input("Number of BHK", min_value=1, max_value=10, step=1)
-bathrooms = st.number_input("Number of Bathrooms", min_value=1, max_value=10, step=1)
+Area = st.number_input("Enter Area (sq ft)", min_value=100, max_value=10000, step=50)
+BHK = st.number_input("Number of BHK", min_value=1, max_value=10, step=1)
+Bathroom = st.number_input("Number of Bathrooms", min_value=1, max_value=10, step=1)
+Location = st.number_input("Enter Area", min_value=100, max_value=10000, step=50)
 
 # Dropdown for location
-location = st.selectbox(
-    "Select Location",
-    ["Delhi", "Noida", "Gurgaon", "Faridabad", "Ghaziabad", "Other"]
-)
+# location = st.selectbox(
+#     "Select Location",
+#     ["Delhi", "Noida", "Gurgaon", "Faridabad", "Ghaziabad", "Other"]
+# )
 
 if st.button("Predict Price"):
     # Prepare input
     input_df = pd.DataFrame({
-        "area": [area],
-        "bhk": [bhk],
-        "bathrooms": [bathrooms],
-        "location": [location]
+        "Area": [Area],
+        "BHK": [BHK],
+        "Bathroom": [Bathroom],
+        "Location": [Location]
     })
     
     # Predict
